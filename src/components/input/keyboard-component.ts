090429a05090ce23cc397136eb27a7a9533447e7
Alvin Haruna <harunaalvin69@gmail.com>
2026-01-26 02:44:59 +0100

    added input and keyboard components with getter & setter functions to track play key input

---
+++ src/components/input/keyboard-component.ts
@@ -0,0 +1,56 @@
+import { InputComponent } from './Input-component';
+
+export class KeyboardComponent extends InputComponent {
+  #cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  #attackKey: Phaser.Input.Keyboard.Key;
+  #actionKey: Phaser.Input.Keyboard.Key;
+  #enterKey: Phaser.Input.Keyboard.Key;
+
+  constructor(keyboardPlugin: Phaser.Input.Keyboard.KeyboardPlugin) {
+    super();
+    this.#cursorKeys = keyboardPlugin.createCursorKeys();
+    this.#attackKey = keyboardPlugin.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+    this.#actionKey = keyboardPlugin.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+    this.#enterKey = keyboardPlugin.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+    // this.#cursorKeys.down.isDown
+  }
+  get isUpDown(): boolean {
+    return this.#cursorKeys.up.isDown;
+  }
+
+  get isDownDown(): boolean {
+    return this.#cursorKeys.down.isDown;
+  }
+
+  get isUpJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#cursorKeys.up);
+  }
+  get isDownJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#cursorKeys.down);
+  }
+
+  get isRightDown(): boolean {
+    return this.#cursorKeys.right.isDown;
+  }
+
+  get isLeftDown(): boolean {
+    return this.#cursorKeys.left.isDown;
+  }
+
+  get isActionKeyJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#actionKey);
+  }
+
+  get isAttackKeyJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#attackKey);
+  }
+
+  get isSelectKeyJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#cursorKeys.shift);
+  }
+
+  get isEnterKeyJustDown(): boolean {
+    return Phaser.Input.Keyboard.JustDown(this.#enterKey);
+  }
+}
