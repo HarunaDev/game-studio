d2a8daef80c4372c6fdac3f24dd5144def1dc0d6
Alvin Haruna <harunaalvin69@gmail.com>
2026-02-16 15:44:33 +0100

    added different player states when interacting with game objects

---
+++ src/components/state-machine/states/character/idle-holding-state.ts
@@ -0,0 +1,31 @@
+import { isArcadePhysicsBody } from '../../../../common/utils';
+import { CharacterGameObject } from '../../../../game-objects/common/character-game-object';
+// import { Player } from '../../../../game-objects/player/player';
+import { BaseCharacterState } from './base-character-state';
+import { CHARACTER_STATES } from './character-states';
+
+export class IdleHoldingState extends BaseCharacterState {
+  constructor(gameObject: CharacterGameObject) {
+    super(CHARACTER_STATES.IDLE_HOLDING_STATE, gameObject);
+  }
+
+  public onEnter(): void {
+    // console.log(this._gameObject.direction);
+    // this._gameObject.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 }, true);
+    this._gameObject.animationComponent.playAnimation(`IDLE_${this._gameObject.direction}`);
+
+    if (isArcadePhysicsBody(this._gameObject.body)) {
+      this._gameObject.body.velocity.x = 0;
+      this._gameObject.body.velocity.y = 0;
+    }
+  }
+  public onUpdate(): void {
+    const controls = this._gameObject.controls;
+
+    if (!controls.isDownDown && !controls.isUpDown && !controls.isLeftDown && !controls.isRightDown) {
+      return;
+    }
+
+    this._stateMachine.setState(CHARACTER_STATES.MOVE_STATE);
+  }
+}
