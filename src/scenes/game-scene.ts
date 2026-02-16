21701f27706a7ce751c2c2c879b41dafff29730a
Alvin Haruna <harunaalvin69@gmail.com>
2026-02-16 02:37:44 +0100

    added pot class

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -8,6 +8,7 @@ import { Wisp } from '../game-objects/enemies/wisp';
 import { DIRECTION } from '../common/common';
 import { CharacterGameObject } from '../game-objects/common/character-game-object';
 import { PLAYER_START_MAX_HEALTH } from '../common/config';
+import { Pot } from '../game-objects/objects/pot';
 
 export class GameScene extends Phaser.Scene {
   #controls!: KeyboardComponent;
@@ -55,6 +56,11 @@ export class GameScene extends Phaser.Scene {
         runChildUpdate: true,
       },
     );
+
+    new Pot({
+      scene: this,
+      position: { x: this.scale.width / 2 + 90, y: this.scale.height / 2 },
+    });
     this.#registerColliders();
   }
 
