75b72fecc607bc8ad78ad751b2131885fde3aeb2
Alvin Haruna <harunaalvin69@gmail.com>
2026-03-27 12:31:32 +0100

    added disable and enable object functions

--- src/game-objects/objects/pot.ts
+++ src/game-objects/objects/pot.ts
@@ -30,12 +30,27 @@ export class Pot extends Phaser.Physics.Arcade.Sprite {
       this.break();
     });
   }
+
+  // disable physics body
+  public disableObject(): void {
+    (this.body as Phaser.Physics.Arcade.Body).enable = false;
+    this.active = false;
+    this.visible = false;
+  }
+
+  // enable physics body
+  public enableObject(): void {
+    (this.body as Phaser.Physics.Arcade.Body).enable = true;
+    this.active = true;
+    this.visible = true;
+  }
+
   public break(): void {
     (this.body as Phaser.Physics.Arcade.Body).enable = false;
     this.setTexture(ASSET_KEYS.POT_BREAK, 0).play(ASSET_KEYS.POT_BREAK);
     this.once(Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + ASSET_KEYS.POT_BREAK, () => {
-      // this.setTexture(ASSET_KEYS.POT, 0);
-      console.log('hide');
+      this.setTexture(ASSET_KEYS.POT, 0);
+      this.disableObject();
     });
   }
 }
