8542f604640ba6284590e7c4cb5747932c7dea6b
Alvin Haruna <harunaalvin69@gmail.com>
2026-04-03 12:21:51 +0100

    modified pot lifting animation

--- src/components/state-machine/states/character/lift-state.ts
+++ src/components/state-machine/states/character/lift-state.ts
@@ -36,6 +36,36 @@ export class LiftState extends BaseCharacterState {
     }
     gameObjectBeingPickedUp.setDepth(2).setOrigin(0.5, 0.5);
 
+    // create curve and path for throw animation
+    const startPoint = new Phaser.Math.Vector2(gameObjectBeingPickedUp.x + 8, gameObjectBeingPickedUp.y - 8);
+    const controlPoint1 = new Phaser.Math.Vector2(gameObjectBeingPickedUp.x + 8, gameObjectBeingPickedUp.y - 24);
+    const controlPoint2 = new Phaser.Math.Vector2(gameObjectBeingPickedUp.x + 8, gameObjectBeingPickedUp.y - 24);
+    const endPoint = new Phaser.Math.Vector2(this._gameObject.x, this._gameObject.y - 8);
+    const curve = new Phaser.Curves.CubicBezier(startPoint, controlPoint1, controlPoint2, endPoint);
+    const curvePath = new Phaser.Curves.Path(startPoint.x, startPoint.y).add(curve);
+
+    // create graphics object to draw out path
+    const g = this._gameObject.scene.add.graphics();
+    g.clear();
+    // g.lineStyle(4, 0x00ff00, 1);
+    curvePath.draw(g);
+    gameObjectBeingPickedUp.setAlpha(0);
+
+    // update game object to follow that path
+    const follower = this._gameObject.scene.add
+      .follower(curvePath, startPoint.x, startPoint.y, gameObjectBeingPickedUp.texture)
+      .setAlpha(1);
+    follower.startFollow({
+      delay: 0,
+      duration: 250,
+      onComplete: () => {
+        follower.destroy();
+        g.destroy();
+
+        gameObjectBeingPickedUp.setPosition(follower.x, follower.y).setAlpha(1);
+      },
+    });
+
     this._gameObject.animationComponent.playAnimation(`LIFT_${this._gameObject.direction}`);
   }
   public onUpdate(): void {
